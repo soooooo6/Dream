b416b1a9e8cd1b49c0ed318287e6f24bc080405c
soooooo6 <Qjdclsp@gmail.com>
2015-05-28 20:25:47 +0900

    2
    2

---
+++ Dream/app/src/main/java/kr/ac/kyonggi/dream/dream/ServerConnector.java
@@ -0,0 +1,71 @@
+package kr.ac.kyonggi.dream.dream;
+
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by samsung on 2015-05-28.
+ */
+
+ class ServerConnector extends AsyncTask<Void, Void, String> {
+        protected String doInBackground(Void... unused) {
+            String content = executeClient();
+            return content;
+        }
+
+        protected void onPostExecute(String result) {
+            // 모두 작업을 마치고 실행할 일 (메소드 등등)
+        }
+
+        // 실제 전송하는 부분
+        public String executeClient() {
+            int index = 0;
+            int count = 20;
+            String url = "http://1.224.193.99:9080/public_html/dream/RestaListJson.php?index="+index+"&count=" + count;
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(url));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
+            }
+            return responseString;
+        }
+    }
+
--- Dream/app/src/main/java/kr/ac/kyonggi/dream/dream/View_list.java
+++ Dream/app/src/main/java/kr/ac/kyonggi/dream/dream/View_list.java
@@ -2,29 +2,29 @@ package kr.ac.kyonggi.dream.dream;
 
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpServerConnection;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
-import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import java.io.InputStreamReader;
 
 
 public class View_list extends ActionBarActivity {
@@ -32,8 +32,6 @@ public class View_list extends ActionBarActivity {
 
     private ListView m_ListView;
     private ArrayAdapter<String> m_Adapter;
-
-    private static final String USER_AGENT = "Mozilla/5.0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,68 +42,18 @@ public class View_list extends ActionBarActivity {
         String str = intent.getStringExtra("PARAM1");
         this.setTitle(intent.getStringExtra("TITLE"));
         TextView txtOutput = (TextView)findViewById(R.id.vl_textView1);
-
-        JSONParser parse = new JSONParser();
-
-//        txtOutput.setText(str+" 검색한다.");
-
-//        JSONArray jsonArr = null;
-
-//        jsonArr.get("id");
-
-  /*      try {
-            jsonArr = doGet("http://create32.ddns.net:9080/public_html/dream/RestaListJson.php?index=0&count=10");
-            txtOutput.setText((CharSequence) jsonArr);
-            Log.e("json", String.valueOf(jsonArr));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-        Button btn = (Button)findViewById(R.id.vl_btnCancle);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //String str = editText1.getText().toString();
-                //Intent resultIntent = new Intent();
-                //resultIntent.putExtra("MY_VALUE", str);
-                //setResult(RECEIVE_EVENT, resultIntent);
-                finish();
-            }
-        });*/
-    }
-
-    /*
-    public JSONArray doGet(String url) throws JSONException {
+        txtOutput.setText(str);
+        String aa = null;
         try {
-            HttpGet method = new HttpGet(url);
-            DefaultHttpClient client = new DefaultHttpClient();
-            // 헤더 설정
-            method.setHeader("Connection", "Keep-Alive");
-
-            HttpResponse response = client.execute(method);
-            // response status 가 400이 아니라면
-            int status = response.getStatusLine().getStatusCode();
-            if( status != HttpStatus.SC_OK ) {
-                Log.e("status", "error");
-                throw new Exception("");    // 실패
-            }
-            // response 받기 JSONArray 로 파싱
-            String str = EntityUtils.toString(response.getEntity(), "UTF-8");
-            System.out.println(str);
-            return new JSONArray(str);
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+            aa = new ServerConnector().executeClient();
+            Log.d("Resta : : : ",aa);
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
         }
+        Toast.makeText(getApplicationContext(), aa, Toast.LENGTH_LONG).show();
     }
-*/
+    
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
